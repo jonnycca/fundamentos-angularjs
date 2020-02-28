8a0e6944540658d5adc2f01a2551296e771db450
Jonny Costa Alves <jcostaal@everis.com>
2020-02-28 12:22:43 -0300

    adicionando filtro para filtrar os numeros do pokemons

--- pokedex/js/services/PokeApiFactory.js
+++ pokedex/js/services/PokeApiFactory.js
@@ -4,10 +4,10 @@ angular.module('pokedexApp')
 function PokeApiFactory(){
     return{
         pkmList: [
-            {name: 'bulbasaur', number: '001'},
-            {name: 'bulbasaur', number: '001'},
-            {name: 'bulbasaur', number: '001'},
-            {name: 'bulbasaur', number: '001'}
+            {name: 'bulbasaur', number: 1},
+            {name: 'bulbasaur', number: 2},
+            {name: 'bulbasaur', number: 3},
+            {name: 'bulbasaur', number: 4}
         ]
     }
 }
