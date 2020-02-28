5235cbdfcd7d4b38c924d81f334c45dae1099f38
Jonny Costa Alves <jcostaal@everis.com>
2020-02-28 15:19:24 -0300

    obtendo os pokemons atraves de requisicao no servidor

--- pokedex/js/controllers/PokeDexController.js
+++ pokedex/js/controllers/PokeDexController.js
@@ -11,5 +11,14 @@ function PokeDexController(PokeApiFactory){
     //variavel que será usada para efetuarmos pesquisa
     vm.searchText = '';
 
-    vm.pkmList = PokeApiFactory.pkmList;
+    vm.pkmList = []; //lista de pokemons vai comecar vazia
+
+    if(vm.pkmList.length){//verificando se a requisicao ja foi feita e ja obteve os dados dos pokemons
+        vm.pkmList = PokeApiFactory.pkmList; //se ja foi feita, ? s? atribuir
+    }else{
+        PokeApiFactory.listAll() //se ainda nao foi feita, chama o listAll para fazer a requisicao e atribui a lista de pokemons
+        .then(pkmList =>{
+            vm.pkmList = pkmList; //obtendo a lista de pokemons que veio da pokeAPI
+        })
+    } 
 }
--- pokedex/js/services/PokeApiFactory.js
+++ pokedex/js/services/PokeApiFactory.js
@@ -1,13 +1,32 @@
 angular.module('pokedexApp')
 .factory('PokeApiFactory', PokeApiFactory);
 
-function PokeApiFactory(){
+PokeApiFactory.$inject = ['$http'] //injetando dependencia do $http
+
+function PokeApiFactory($http){
     return{
-        pkmList: [
-            {name: 'bulbasaur', number: 1},
-            {name: 'bulbasaur', number: 2},
-            {name: 'bulbasaur', number: 3},
-            {name: 'bulbasaur', number: 4}
-        ]
+        get url(){
+            return '//dev.treinaweb.com.br/pokeapi/' //url em que buscara os dados dos pokemons
+        },
+        pkmList: [], //iniciando nossa lista de pokemons vazia
+        listAll: function(){
+            return $http.get(`${this.url}pokedex/1`) //service http fazendo requisicao
+                .then(response => response.data.pokemon) //obtendo a resposta da requisicao, pegando o atributo pokemon
+                .then(pkmList => {
+                    return pkmList.map(pokemon => { //mapeando cada pokemon com seu nuemro
+                        pokemon.number = this.getNumberFromURL(pokemon.resource_uri); 
+                        return pokemon;
+                    })
+                    .filter(pokemon => pokemon.number < 1000)
+                    .sort((a, b) => (a.number > b.number ? 1 : -1))
+                })
+                .then(pkmList =>{
+                    this.pkmList = pkmList;
+                    return pkmList; //retornando a lista de pokemons
+                })
+        },
+        getNumberFromURL: function(url){
+            return parseInt(url.replace(/.*\/(\d+)\/$/, '$1')); //formatando o numero do pokemon que veio da url
+        }
     }
 }
